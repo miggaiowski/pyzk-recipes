07fade6fb38fce19f523066efe10eb668198cd4a
Miguel Gaiowski <miggaiowskI@gmail.com>
2011-01-31 14:31:22 -0200

    Teste

--- queue.py
+++ queue.py
@@ -23,6 +23,8 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# Grupo 03
+
 import zookeeper, threading, sys, time
 ZOO_OPEN_ACL_UNSAFE = {"perms":0x1f, "scheme":"world", "id" :"anyone"};
 
