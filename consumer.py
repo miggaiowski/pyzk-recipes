005b84b2e4919878e66e4f875c2b8422ed9dc229
Miguel Gaiowski <miggaiowskI@gmail.com>
2011-01-31 15:59:37 -0200

    This guy consumes the items from the queue. That's pretty much it.

---
+++ consumer.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2010, Henry Robinson
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Grupo 03
+
+import zookeeper, threading, sys, time, queue
+ZOO_OPEN_ACL_UNSAFE = {"perms":0x1f, "scheme":"world", "id" :"anyone"};
+
+if __name__ == '__main__':
+  if len(sys.argv) < 3:
+    print "Usage: python", sys.argv[0], "PORTNUMBER", "ID"
+    sys.exit(1)
+  zk = queue.ZooKeeperQueue("myfirstqueue", int(sys.argv[1]))
+  ID = int(sys.argv[2])
+
+  try:
+    v = zk.dequeue()
+    while v != None:
+      print "Consumer %d: %s" % (ID, v)
+      sys.stdout.flush()
+      time.sleep(0.1)
+      v = zk.dequeue()
+    print "Nothing to be consumed"
+  except KeyboardInterrupt:
+    pass
+  zk.__del__()
+  print "Done"
