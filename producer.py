8a8c458fb73b632110f1431cd13c2de936e0428e
Miguel Gaiowski <miggaiowskI@gmail.com>
2011-01-31 16:01:27 -0200

    This one will queue items until we have 10 extra items after the next to be dequeued item. That is, it will try to maintain a buffer of 10 items in the queue.
    Sleeping time may be ajusted (in the consumer as well)
    to allow more consumers to eat items without the producer
    being too slow do queue new ones.

---
+++ producer.py
@@ -0,0 +1,52 @@
+# Copyright (c) 2010, Henry Robinson
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the <organization> nor the
+#       names of its contributors may be used to endorse or promote products
+#       derived from this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Grupo 03
+
+import zookeeper, threading, sys, time, queue
+
+NUMPRODUCTS = 10
+
+if __name__ == '__main__':
+  if len(sys.argv) < 2:
+    print "Usage: python " + sys.argv[0] + " PORTNUMBER"
+    sys.exit(1)
+  zk = queue.ZooKeeperQueue("myfirstqueue", int(sys.argv[1]), is_producer=True)
+  try:
+    lastproduct = 0
+    while True:
+      # Enqueueing new items, until we have a buffer of NUMPRODUCTS
+      next_item = zk.get_and_maintain()
+      while not next_item or lastproduct - int(next_item) < NUMPRODUCTS:
+        lastproduct += 1
+        zk.enqueue("%d" % lastproduct)
+        print "Enqueued %d" % lastproduct
+        next_item = zk.get_and_maintain()
+        time.sleep(0.04)
+  except KeyboardInterrupt:
+    pass
+  zk.__del__()
+  print "Done"
+  
